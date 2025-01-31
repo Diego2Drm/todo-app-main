55a00267f0ad989d7d5f2bd7e0b6d0244e35eaec
Diego Ramirez <diego.ramirez2d03@gmail.com>
2025-01-30 21:36:51 -0600

    Project Initialization and Tailwind Configuration

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="mt-5 text-3xl font-bold  font-josefin uppercase w-full text-Bright-Blue">Todo App veamos si funciona debe de tener un break al </h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        /* Primary */
+        'Bright-Blue': ' #3a7bfd',
+        'Check-Background': 'linear-gradient #57ddff to #c058f3',
+
+        /* Light Theme */
+        'Very-Light-Gray': '#fafafa',
+        ' Very-Light-Grayish-Blue': '#e4e5f1',
+        'Light-Grayis-Blue': '#d2d3db',
+        'Dark-Grayish-Blue': '#9394a5',
+        'Very-Dark-Grayish-Blue1': '#484b6a',
+
+        /* Dark Theme */
+        'Very-Dark-Blue': '#161722',
+        'Very-Dark-Desaturated-Blue': '#25273c',
+        ' Light-Grayish-Blue': '#cacde8',
+        'Light-Grayish-Blue-hover': '#e4e5f1', /* (hover) */
+        'Dark-Grayish-Blue': '#777a92',
+        'Very-Dark-Grayish-Blue2': '#4d5066',
+        'Very-Dark-Grayish-Blue3': '#393a4c',
+      },
+      fontFamily: {
+        /* Fonts */
+        'josefin':[ "Josefin Sans", "serif"],
+      }
+    },
+  },
+  plugins: [],
+}
+
